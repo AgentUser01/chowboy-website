7643e72cd1c288af0a6e378f2f0cdd1964b7d463
Taylor Brown <taylor.brown18@hotmail.com>
2025-10-15 15:18:38 -0400

    Updates

--- next/static/chunks/4fa33490a8d6da8d.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/2d7f6f8f707e5655.js",
-  "static/chunks/d63c53cc4cfe89f0.js",
-  "static/chunks/turbopack-412912952e4f9129.js"
-])
---
+++ next/static/chunks/app/features/page-d5fdf56c9befbc99.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[944],{},_=>{_.O(0,[356,360,441,255,358],()=>_(_.s=360)),_N_E=_.O()}]);
